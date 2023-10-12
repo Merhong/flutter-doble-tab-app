e41ae0f7ec5b21c7441f6d90da89386d908f6d26
Merhong <zx2938@gmail.com>
2023-10-12 16:08:36 +0900

    Material 3 색상 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_double_tab_app/color_schemes.g.dart';
 
 /// Flutter code sample for [TabBar].
 
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      themeMode: ThemeMode.system,
       home: DoubleTabbar(),
     );
   }
@@ -38,22 +42,22 @@ class _DoubleTabbarState extends State<DoubleTabbar>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Flutter TabBar Example ${tabNumber}')),
-        body: Column(
-          children: [
-            _tabbar1(),
-            _tabbar2(),
-            Expanded(
-              child: Stack(
-                children: [
-                  if (tabNumber == 1) _view1(),
-                  if (tabNumber == 2) _view2()
-                ],
-              ),
+      appBar: AppBar(title: Text('Flutter TabBar Example ${tabNumber}')),
+      body: Column(
+        children: [
+          _tabbar1(),
+          _tabbar2(),
+          Expanded(
+            child: Stack(
+              children: [
+                if (tabNumber == 1) _view1(),
+                if (tabNumber == 2) _view2()
+              ],
             ),
-          ],
-        ));
-  }
+          ),
+        ],
+      ),
+    );}
 
   TabBar _tabbar1() {
     return TabBar(
@@ -63,7 +67,7 @@ class _DoubleTabbarState extends State<DoubleTabbar>
         });
       },
       controller: _tabController1,
-      labelColor: Colors.brown, // 라벨의 색상 지정
+      // labelColor: Colors.brown, // 라벨의 색상 지정
       tabs: [
         Tab(icon: Icon(Icons.home), text: 'Home'),
         Tab(icon: Icon(Icons.favorite), text: 'Favorites'),
@@ -80,7 +84,7 @@ class _DoubleTabbarState extends State<DoubleTabbar>
         });
       },
       controller: _tabController2,
-      labelColor: Colors.teal, // 라벨의 색상 지정
+      labelColor: lightColorScheme.secondary, // 라벨의 색상 지정
       tabs: [
         Tab(icon: Icon(Icons.home), text: 'Home2'),
         Tab(icon: Icon(Icons.favorite), text: 'Favorites2'),
@@ -93,9 +97,9 @@ class _DoubleTabbarState extends State<DoubleTabbar>
     return TabBarView(
       controller: _tabController1,
       children: [
-        Center(child: Text('Home')),
-        Center(child: Text('Favorites')),
-        Center(child: Text('Settings')),
+        Center(child: Text('Home 내용')),
+        Center(child: Text('Favorites 내용')),
+        Center(child: Text('Settings 내용')),
       ],
     );
   }
@@ -104,9 +108,9 @@ class _DoubleTabbarState extends State<DoubleTabbar>
     return TabBarView(
       controller: _tabController2,
       children: [
-        Center(child: Text('Home2')),
-        Center(child: Text('Favorites2')),
-        Center(child: Text('Settings2')),
+        Center(child: Text('Home2 내용')),
+        Center(child: Text('Favorites2 내용')),
+        Center(child: Text('Settings2 내용')),
       ],
     );
   }
